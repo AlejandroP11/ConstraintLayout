d1d08b5dd68dfd0a81b9a5c2d009c9386d21e977
AlejandroPereiro <jpereirogabian@danielcastelao.org>
2022-09-26 10:48:50 +0200

    Primeras pruebas

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.constraintlayout
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 }
